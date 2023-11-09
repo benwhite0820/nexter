b0b891d8d20161196a4c8a3754294949c75ec14f
benwhite0820 <dragon7230@gmail.com>
2023-11-09 16:57:36 +0800

    building the features section - part 1

--- components/features/features.component.tsx
+++ components/features/features.component.tsx
@@ -1,7 +1,18 @@
 import './features.style.scss';
+import { featureMockData } from '@/data/featureMockData';
 
 const Features = () => {
-  return <div className="features">features</div>;
+  return (
+    <div className="features">
+      {featureMockData.map(({ title, content, logo }, index) => (
+        <div className="feature" key={index}>
+          <span className="feature__icon">{logo}</span>
+          <h4 className="heading-4">{title}</h4>
+          <p className="feature__text">{content}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Features;
---
+++ data/featureMockData.tsx
@@ -0,0 +1,43 @@
+import { BiWorld } from 'react-icons/bi';
+import { BsTrophy, BsKey, BsLock } from 'react-icons/bs';
+import { CiLocationOn } from 'react-icons/Ci';
+import { HiOutlinePresentationChartLine } from 'react-icons/hi';
+
+export const featureMockData = [
+  {
+    title: `World's best luxury homes`,
+    content:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur voluptatibus.',
+    logo: <BiWorld />,
+  },
+  {
+    title: 'Only the best properties',
+    content:
+      'Voluptatum mollitia quae. Vero ipsum sapiente molestias accusamus rerum sed a eligendi aut quia.',
+    logo: <BsTrophy />,
+  },
+  {
+    title: 'All homes in the top locations',
+    content:
+      'Tenetur distinctio necessitatibus pariatur voluptatibus quidem consequatur harum.',
+    logo: <CiLocationOn />,
+  },
+  {
+    title: 'New home in one week',
+    content:
+      'Vero ipsum sapiente molestias accusamus rerum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+    logo: <BsKey />,
+  },
+  {
+    title: 'Top 1% realtors',
+    content:
+      'Quidem consequatur harum, voluptatum mollitia quae. Tenetur distinctio necessitatibus pariatur voluptatibus.',
+    logo: <HiOutlinePresentationChartLine />,
+  },
+  {
+    title: 'Secure payments on nexter',
+    content:
+      'Pariatur voluptatibus quidem consequatur harum, voluptatum mollitia quae.',
+    logo: <BsLock />,
+  },
+];
