f33aa16776676b81cd258ffc422c6cac72b848a8
benwhite0820 <dragon7230@gmail.com>
2023-11-09 10:28:29 +0800

    project overview and setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
-import styles from './page.module.css';
+import './page.scss';
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return <main>test</main>;
 }
