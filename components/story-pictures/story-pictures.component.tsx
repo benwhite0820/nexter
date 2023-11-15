f5acb6fa27dcf64950e6f1a882a3f93027a539f0
benwhite0820 <dragon7230@gmail.com>
2023-11-15 23:29:36 +0800

    building the story section - part 2

--- components/story-pictures/story-pictures.component.tsx
+++ components/story-pictures/story-pictures.component.tsx
@@ -1,7 +1,17 @@
 import './story-pictures.style.scss';
 
 const StoryPictures = () => {
-  return <div className="story__pictures">story pictures</div>;
+  return (
+    <div className="story__pictures">
+      <img
+        src="/story-1.jpeg"
+        className="story__img--1"
+        alt="Couple with new house"
+      />
+
+      <img src="/story-2.jpeg" className="story__img--2" alt="New house" />
+    </div>
+  );
 };
 
 export default StoryPictures;
