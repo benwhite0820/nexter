af8de4e4771fc67d6bd158cb1da74a24589ba7c2
benwhite0820 <dragon7230@gmail.com>
2023-11-17 23:58:40 +0800

    building the gallery - part 1

--- components/gallery/gallery.component.tsx
+++ components/gallery/gallery.component.tsx
@@ -1,7 +1,21 @@
 import './gallery.style.scss';
 
 const Gallery = () => {
-  return <div className="gallery">gallery</div>;
+  return (
+    <div className="gallery">
+      <figure className="gallery__item gallery__item--1">
+        <img src="/gal-1.jpeg" alt="gallery image 1" className="gallery__img" />
+      </figure>
+      <img src="/gal-2.jpeg" alt="gallery image 2" className="gallery__img" />
+    </div>
+  );
 };
 
 export default Gallery;
+
+<div className="gallery">
+  <figure className="gallery__item gallery__item--1">
+    <img src="/gal-1.jpeg" alt="gallery image 1" className="gallery__img" />
+  </figure>
+  <img src="/gal-2.jpeg" alt="gallery image 2" className="gallery__img" />
+</div>;
