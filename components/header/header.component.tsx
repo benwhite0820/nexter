4342fa0effff2c428936dd897fc230d12bb97b87
benwhite0820 <dragon7230@gmail.com>
2023-11-18 20:00:18 +0800

    building the header - part 1

--- components/header/header.component.tsx
+++ components/header/header.component.tsx
@@ -1,7 +1,23 @@
+import Button from '../button/button.component';
+import Heading from '../heading/heading.component';
 import './header.style.scss';
 
 const Header = () => {
-  return <header className="header">header</header>;
+  return (
+    <header className="header">
+      <img src="/logo.png" alt="Nexter logo" className="header__logo" />
+      <Heading headingLevel="h3">Your own home</Heading>
+      <Heading headingLevel="h1">The ultimate personal freedom</Heading>
+      <Button className="header__btn">View our properties</Button>
+      <div className="header__seenon-text">Seen on</div>
+      <div className="header__seenon-logos">
+        <img src="/logo-bbc.png" alt="Seen on logo 1" />
+        <img src="/logo-forbes.png" alt="Seen on logo 2" />
+        <img src="/logo-techcrunch.png" alt="Seen on logo 3" />
+        <img src="/logo-bi.png" alt="Seen on logo 4" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
