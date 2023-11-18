d944d2f3e2fa92e0c0172db7e7f672ce1b9b12da
benwhite0820 <dragon7230@gmail.com>
2023-11-18 22:17:40 +0800

    building the realtors section

--- components/realtors/realtors.component.tsx
+++ components/realtors/realtors.component.tsx
@@ -1,7 +1,35 @@
+import Heading from '../heading/heading.component';
 import './realtors.style.scss';
 
 const Realtors = () => {
-  return <div className="realtors">top 3 realtors</div>;
+  return (
+    <div className="realtors">
+      <Heading headingLevel="h3">Top 3 Realtors</Heading>
+      <div className="realtors__list">
+        <img src="/realtor-1.jpeg" alt="Realtor 1" className="realtors__img" />
+        <div className="realtors__details">
+          <Heading headingLevel="h4" color="light">
+            Erik Feinman
+          </Heading>
+          <p className="realtors__sold">245 houses sold</p>
+        </div>
+        <img src="/realtor-2.jpeg" alt="Realtor 2" className="realtors__img" />
+        <div className="realtors__details">
+          <Heading headingLevel="h4" color="light">
+            Kim Brown
+          </Heading>
+          <p className="realtors__sold">212 houses sold</p>
+        </div>
+        <img src="/realtor-3.jpeg" alt="Realtor 3" className="realtors__img" />
+        <div className="realtors__details">
+          <Heading headingLevel="h4" color="light">
+            Toby Ramsey
+          </Heading>
+          <p className="realtors__sold">198 houses sold</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Realtors;
